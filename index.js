3c501a256a7a7f21ea793a6040abf7b2a0c1c4fe
aunishukri <b022210115@student.utem.edu.my>
2025-01-06 15:32:28 +0800

    7

--- index.js
+++ index.js
@@ -233,19 +233,75 @@ app.post('/login', async (req, res) => {
   }
 });
 
-
 // Get user profile
 app.get('/user/:id', verifyToken, async (req, res) => {
   if (req.identity._id != req.params.id) {
-    res.status(401).send('Unauthorized Access');
+    return res.status(401).send('Unauthorized access');
+  }
+
+  let result = await client.db("user").collection("userdetail").findOne({
+    _id: new ObjectId(req.params.id)
+  });
+  res.send(result);
+});
+
+
+app.post('/buy', async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  var decoded = jwt.verify(token, 'mysupersecretpasskey');
+  console.log(decoded);
+});
+
+// Choose map - Authenticated route
+app.post('/choose-map', verifyToken, (req, res) => {
+  const selectedMapName = req.body.selectedMap;
+
+  function mapJsonPathExists(mapPath) {
+    try {
+      fs.accessSync(mapPath, fs.constants.F_OK);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  const mapJsonPath = `./${selectedMapName}.json`;
+
+  if (mapJsonPathExists(mapJsonPath)) {
+    const mapData = require(mapJsonPath);
+    selectedMap = selectedMapName; // Store the selected map globally
+    playerPosition = mapData.playerLoc; // Set initial player position
+    const room1Message = mapData.map.room1.message;
+
+    res.send(`You chose ${selectedMapName}. Let's start playing!\n\nRoom 1 Message:\n${room1Message}`);
   } else {
-    let result = await client.db("user").collection("userdetail").findOne({
-      _id: new ObjectId(req.params.id)
-    });
-    res.send(result);
+    res.status(404).send(`Map "${selectedMapName}" not found.`);
   }
 });
 
+// Move - Authenticated route
+app.patch('/move', verifyToken, (req, res) => {
+  const direction = req.body.direction;
+
+  if (!selectedMap) {
+    res.status(400).send("No map selected.");
+    return;
+  }
+
+  const mapData = require(`./${selectedMap}.json`);
+  const currentRoom = mapData.map[playerPosition];
+
+  const nextRoom = currentRoom[direction];
+  if (!nextRoom) {
+    res.status(400).send(`Invalid direction: ${direction}`);
+    return;
+  }
+
+  const nextRoomMessage = mapData.map[nextRoom].message;
+  playerPosition = nextRoom;
+
+  res.send(`You moved ${direction}. ${nextRoomMessage}`);
+});
 
 // Start the server
 app.listen(port, () => {
